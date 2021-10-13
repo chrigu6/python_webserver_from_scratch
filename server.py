a1efa361dfbc028dfcb63a75947b1bff483574a0
Christoph Reinhart <christoph.reinhart2@gmail.com>
2021-10-13 18:02:35 +0200

    Initial commit

---
+++ server.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 9000
+
+with socket.socket() as server_sock:
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_sock.bind((HOST, PORT))
+
+    server_sock.listen(0)
+    print(f"Listening on {HOST}:{PORT}...")
