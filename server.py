05b4bce23f1ef7f7a829653794f4fbf773ab4f17
Christoph Reinhart <christoph.reinhart2@gmail.com>
2021-10-13 18:12:48 +0200

    Sending Response

--- server.py
+++ server.py
@@ -2,14 +2,20 @@ import socket
 
 HOST = "127.0.0.1"
 PORT = 9000
+RESPONSE = b""""\
+HTTP/1.1 200 OK
+Content-type: text/html
+Content-length: 15
+
+<h1>Hello!</h1>""".replace(b"\n", b"\r\n")
 
 with socket.socket() as server_sock:
     server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
     server_sock.bind((HOST, PORT))
-
     server_sock.listen(0)
     print(f"Listening on {HOST}:{PORT}...")
 
     client_sock, client_addr = server_sock.accept()
     print(f"New connection from {client_addr}.")
+    with client_sock:
+        client_sock.sendall(RESPONSE)
